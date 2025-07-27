1fcb362232c227e7b79575b9589e6d7ee9442a63
Ishan2103 <ishanextra21@gmail.com>
2025-07-27 19:58:06 +0530

    Add files via upload

---
+++ 1_to_20_for_loop.cpp
@@ -0,0 +1,10 @@
+//Name : Ishan 
+//PRN : 24070123047
+//Batch : ENTC A2
+#include <iostream>
+using namespace std;
+int main(){
+    int i=0;
+    while(i<20){cout<<i+1<<" ";i++;cout<<endl;}
+    return 0;
+}
