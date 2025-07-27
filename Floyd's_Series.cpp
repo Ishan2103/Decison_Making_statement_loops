c4dd5fcf04bf8860adf99d1b0d44b18c84929c1d
Ishan2103 <ishanextra21@gmail.com>
2025-07-27 20:10:45 +0530

    Add files via upload

---
+++ Floyd's_Series.cpp
@@ -0,0 +1,17 @@
+//Name : Ishan Danech
+//PRN : 24070123047
+//Batch : ENTC A2
+#include <iostream>
+using namespace std;
+int main() {
+    int rows,num=1;
+    cout<<"Enter number of rows: ";
+    cin>>rows;
+    for(int i=1;i<=rows;i++) {
+        for(int j=1;j<=i;j++) {
+            cout<<num<<" ";
+            num++;
+        }
+        cout << endl;}
+    return 0;
+}
