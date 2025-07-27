40651d2d29be2bcac3ab195c8ac94ac87d1c8ac7
Ishan2103 <ishanextra21@gmail.com>
2025-07-27 21:09:33 +0530

    Add files via upload

---
+++ 0 to 10 using for loop.cpp
@@ -0,0 +1,12 @@
+//Name : Ishan Danech
+//PRN : 24070123047
+//Batch : ENTC A2
+#include <iostream>
+using namespace std;
+
+int main(){
+    for (int i=0;i<=10;i++) 
+    {if(i%2==0){cout<<i<<endl;}
+    }
+    return 0;
+}
