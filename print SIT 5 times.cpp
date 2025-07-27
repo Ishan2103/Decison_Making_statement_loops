939da995d2efac28dbf93f26e8a2fb2dd1d55c2f
Ishan2103 <ishanextra21@gmail.com>
2025-07-27 21:19:11 +0530

    Add files via upload

---
+++ print SIT 5 times.cpp
@@ -0,0 +1,12 @@
+// Name : Ishan Danech
+//PRN : 24070123047
+//Batch : ENTC A2
+#include <iostream>
+using namespace std;
+
+int main(){
+    for (int i=0;i<=5;i++) 
+    {cout<<"SIT"<<endl;}
+    
+    return 0;
+}
